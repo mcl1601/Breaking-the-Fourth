c72117ca6af67a4f93fa459364e1d3f01df3cb99
Mike O'Donnell <mxo7550@rit.edu>
2016-03-11 15:21:22 -0500

    Added terrrain

--- BreakingTheFourth/BreakingTheFourth/Terrain.cs
+++ BreakingTheFourth/BreakingTheFourth/Terrain.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace BreakingTheFourth
 {
@@ -11,5 +14,24 @@ namespace BreakingTheFourth
         //Are we gonna need Intersect methods for both the player and the ground? Seems redundant.
         //If we're gonna make the level move around the player, we'll need X and Y coordinates to move. How does that even work?
         //Do we make the ground a Rectangle? This will probably be the trickiest class.
+        Texture2D image;
+        Rectangle position;
+        public Terrain(int x, int y, int width, int height)
+        {
+            position = new Rectangle(x, y, width, height);
+        }
+        public Texture2D Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        public Rectangle Position
+        {
+            get { return position; }
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(image, position, Color.White);
+        }
     }
 }
