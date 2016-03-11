acab3fabfe6093e3a572d3e4af4dfe7e60d9da59
Katarina Weis (RIT Student) <kxw4589@ad.rit.edu>
2016-03-11 15:22:48 -0500

    basic terrain stuff

--- BreakingTheFourth/BreakingTheFourth/Terrain.cs
+++ BreakingTheFourth/BreakingTheFourth/Terrain.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//added namespaces
+using Microsoft.Xna.Framework;
 
 namespace BreakingTheFourth
 {
@@ -11,5 +13,20 @@ namespace BreakingTheFourth
         //Are we gonna need Intersect methods for both the player and the ground? Seems redundant.
         //If we're gonna make the level move around the player, we'll need X and Y coordinates to move. How does that even work?
         //Do we make the ground a Rectangle? This will probably be the trickiest class.
+
+        //fields
+        Rectangle position;
+        //method for collision detection for player and bullet
+        /*public bool CollisionDetected(Rectangle entity)
+        {
+            if()
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }*/
     }
 }
