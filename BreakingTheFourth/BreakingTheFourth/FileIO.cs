50db126a382c6e86fbd1743e4fff6844aba6dcd3
Matthew Lienhard (RIT Student) <mcl1601@ad.rit.edu>
2016-03-11 15:48:25 -0500

    Began adding the code for reading in from the tool

---
+++ BreakingTheFourth/BreakingTheFourth/FileIO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BreakingTheFourth
+{
+    class FileIO
+    {
+        // fields
+        int playerSpeed;
+        int bulletSpeed;
+        int gravity;
+        
+        // reading in the file
+        public FileIO()
+        {
+            // make the stream
+            Stream inStream = File.OpenRead("../../../../Tool/Tool/bin/Debug/Values.data");
+
+            // read the file
+            BinaryReader input = new BinaryReader(inStream);
+
+            
+
+            // assign the values when reading
+            playerSpeed = input.ReadInt32();
+            bulletSpeed = input.ReadInt32();
+            gravity = input.ReadInt32();
+
+            // close the file
+            input.Close();
+        }
+
+        // properties
+    }
+}
