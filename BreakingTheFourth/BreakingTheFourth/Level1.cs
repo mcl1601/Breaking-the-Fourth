5e4a994244fa40bd1041e634d187deb83de00fbd
Mike O'Donnell <mxo7550@rit.edu>
2016-04-14 00:31:12 -0400

    Added first 3 screens.

--- BreakingTheFourth/BreakingTheFourth/Level1.cs
+++ BreakingTheFourth/BreakingTheFourth/Level1.cs
@@ -28,35 +28,36 @@ namespace BreakingTheFourth
                     // clear the list
                     pieces.Clear();
                     // add new terrain pieces
-                    pieces.Add(new Terrain(0, 0, 800, 40));
-                    pieces.Add(new Terrain(0, 450, 800, 40));
-                    pieces.Add(new Terrain(0, 0, 25, 500));
-                    pieces.Add(new Terrain(775, 0, 25, 300));
-                    pieces.Add(new Terrain(150, 200, 70, 100));
-                    pieces.Add(new Terrain(50, 150, 50, 50));
-                    pieces.Add(new SpecialTerrain(400, 399, 75, 75, 300, 450));
-                    pieces.Add(new DeathObject(500, 420, 60, 30));
+                    pieces.Add(new Terrain(0, 0, 800, 40)); //top
+                    pieces.Add(new Terrain(0, 450, 400, 40)); // left floor
+                    pieces.Add(new Terrain(0, 0, 25, 500)); // left wall
+                    pieces.Add(new Terrain(775, 0, 25, 300)); // right wall
+                    pieces.Add(new Terrain(475, 450, 400, 40)); // right floor
                     break;
                 case 2:
                     // clear the list
                     pieces.Clear();
                     // add new terrain pieces
-                    pieces.Add(new Terrain(0, 0, 800, 40)); 
-                    pieces.Add(new Terrain(0, 450, 800, 40));
-                    pieces.Add(new Terrain(0, 0, 25, 300));
-                    pieces.Add(new Terrain(775, 0, 25, 300));
-                    pieces.Add(new Terrain(500, 430, 100, 10));
+                    pieces.Add(new Terrain(0, 0, 800, 40)); //top
+                    pieces.Add(new Terrain(0, 450, 150, 40)); // left floor
+                    pieces.Add(new Terrain(0, 0, 25, 300)); // left wall
+                    pieces.Add(new Terrain(775, 0, 25, 300)); // right wall
+                    pieces.Add(new Terrain(550, 450, 400, 40)); // right floor
                     break;
                 case 3:
                     // clear the list
                     pieces.Clear();
                     // add new pieces
-                    pieces.Add(new Terrain(0, 0, 800, 40)); //top
-                    pieces.Add(new Terrain(0, 450, 300, 40)); // left floor
-                    pieces.Add(new Terrain(0, 0, 25, 300)); // left wall
-                    pieces.Add(new Terrain(775, 0, 25, 500)); // right wall
-                    pieces.Add(new Terrain(475, 450, 400, 40)); // right floor
-                    pieces.Add(new SpecialTerrain(350, 450, 75, 40, 250, 470)); // moving platform
+                    pieces.Add(new Terrain(0, 0, 25, 300)); // left wall 
+                    pieces.Add(new Terrain(750, 0, 50, 110)); // right wall(upper)
+                    pieces.Add(new Terrain(750, 200, 50, 250)); // right wall (lower)
+                    pieces.Add(new Terrain(710, 200, 40, 50)); //right wall platform
+                    pieces.Add(new Terrain(0, 450, 800, 40)); // floor
+                    pieces.Add(new Terrain(125, 0, 20, 380)); // first obstacle (top)
+                    pieces.Add(new Terrain(125, 425, 20, 25)); // first obstacle (bottom)
+                    pieces.Add(new Terrain(350, 0, 50, 260)); // second obstacle (top)
+                    pieces.Add(new Terrain(350, 350, 50, 100)); //second obstacle (bottom)
+                    pieces.Add(new DeathObject(400, 400, 350, 50)); //spikes
                     break;
             }
             return pieces;
