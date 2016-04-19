d7a6b02eec91283234bf4d15f94f06c98c1ba2f0
Mike O'Donnell <mxo7550@rit.edu>
2016-04-19 18:31:06 -0400

    Made Level 1 easier, put some of the older screens into Level 2. Level 2 still incomplete.

--- BreakingTheFourth/BreakingTheFourth/Level1.cs
+++ BreakingTheFourth/BreakingTheFourth/Level1.cs
@@ -42,6 +42,7 @@ namespace BreakingTheFourth
                     // add new terrain pieces
                     pieces.Add(new Terrain(0, 0, 800, 40)); //top
                     pieces.Add(new Terrain(0, 450, 150, 40)); // left floor
+                    pieces.Add(new Terrain(60, 440, 15, 10)); //thingy to stop you from walking right off a cliff
                     pieces.Add(new Terrain(0, 0, 25, 300)); // left wall
                     pieces.Add(new Terrain(775, 0, 25, 300)); // right wall
                     pieces.Add(new Terrain(550, 450, 400, 40)); // right floor
@@ -50,59 +51,42 @@ namespace BreakingTheFourth
                     // clear the list
                     pieces.Clear();
                     // add new pieces
-                    pieces.Add(new Terrain(0, 0, 25, 300)); // left wall 
-                    pieces.Add(new Terrain(750, 0, 50, 110)); // right wall(upper)
-                    pieces.Add(new Terrain(750, 200, 50, 250)); // right wall (lower)
-                    pieces.Add(new Terrain(710, 200, 40, 50)); //right wall platform
-                    pieces.Add(new Terrain(0, 450, 800, 40)); // floor
-                    pieces.Add(new Terrain(125, 0, 20, 380)); // first obstacle (top)
-                    pieces.Add(new Terrain(125, 425, 20, 25)); // first obstacle (bottom)
-                    pieces.Add(new Terrain(350, 0, 50, 260)); // second obstacle (top)
-                    pieces.Add(new Terrain(350, 350, 50, 100)); //second obstacle (bottom)
-                    pieces.Add(new DeathObject(400, 400, 350, 50)); //spikes
+                    pieces.Add(new Terrain(0, 0, 25, 300)); // left wall
+                    pieces.Add(new Terrain(0, 450, 800, 40)); //floor
+                    pieces.Add(new DeathObject(350, 410, 100, 40)); //spikes
+                    pieces.Add(new Terrain(350, 0, 100, 40)); //ceiling
+                    pieces.Add(new Terrain(350, 225, 100, 40)); //platform
                     break;
                 case 4:
                     // clear the list
                     pieces.Clear();
                     // add new pieces
-                    pieces.Add(new Terrain(0, 450, 150, 40)); // left floor
                     pieces.Add(new Terrain(0, 0, 25, 300)); // left wall
-                    pieces.Add(new SpecialTerrain(200, 440, 60, 60, 200, 440)); //moving platform
-                    pieces.Add(new Terrain(275, 0, 40, 200)); //first obstacle (top)
-                    pieces.Add(new Terrain(275, 260, 40, 60)); //first obstacle (middle)
-                    pieces.Add(new Terrain(275, 380, 40, 100)); //first obstacle (bottom)
-                    pieces.Add(new Terrain(470, 200, 20, 60)); //top floating platform (vertical)
-                    pieces.Add(new Terrain(370, 260, 120, 20)); //top floating platform (horizontal)
-                    pieces.Add(new Terrain(400, 320, 20, 60)); //bottom floating platform
-                    pieces.Add(new Terrain(500, 0, 800, 40)); //ceiling
-                    pieces.Add(new Terrain(500, 120, 40, 300)); // giant floating wall in the middle, ya can't miss it
-                    pieces.Add(new Terrain(500, 450, 350, 40)); //right floor
-                    pieces.Add(new Terrain(700, 200, 60, 30)); //top spike platform
-                    pieces.Add(new Terrain(540, 250, 80, 30)); //bottom spike platform
-                    pieces.Add(new Terrain(760, 0, 50, 230)); //right wall (top)
-                    pieces.Add(new Terrain(760, 380, 50, 400)); //right wall (bottom)
-                    pieces.Add(new Terrain(720, 380, 40, 20)); //bottom right wall cliff
-                    pieces.Add(new DeathObject(700, 170, 60, 30)); //top spikes
-                    pieces.Add(new DeathObject(540, 220, 80, 30)); //middle spikes
-                    pieces.Add(new DeathObject(500, 420, 260, 30)); //bottom spikes
+                    pieces.Add(new Terrain(0, 450, 800, 40)); //floor
+                    pieces.Add(new Terrain(60, 440, 15, 10)); //thingy to stop you from walking right onto the spikes
+                    pieces.Add(new DeathObject(140, 410, 570, 40)); //floor spikes
+                    pieces.Add(new Terrain(0, 0, 800, 40)); //ceiling
+                    pieces.Add(new Terrain(100, 225, 75, 40)); //first platform
+                    pieces.Add(new Terrain(225, 140, 75, 40)); // 1st top platform
+                    pieces.Add(new DeathObject(225, 100, 75, 40)); //1st top platform spikes
+                    pieces.Add(new Terrain(225, 325, 75, 40)); //1st bottom platform
+                    pieces.Add(new Terrain(550, 140, 75, 40)); //2nd top platform
+                    pieces.Add(new Terrain(550, 325, 75, 40)); //2nd bottom platform
+                    pieces.Add(new DeathObject(550, 285, 75, 40)); //2nd bottom platform spikes 
+                    pieces.Add(new Terrain(375, 0, 40, 195)); //divider
                     break;
                 case 5:
                     //clear the list
                     pieces.Clear();
                     //add new pieces
-                    pieces.Add(new Terrain(0, 450, 225, 40)); // left floor
                     pieces.Add(new Terrain(0, 0, 25, 300)); // left wall
-                    pieces.Add(new Terrain(775, 0, 25, 500)); // right wall
-                    pieces.Add(new Terrain(225, 150, 50, 500)); //big wall
+                    pieces.Add(new Terrain(0, 450, 150, 40)); //left floor
                     pieces.Add(new Terrain(0, 0, 800, 40)); //ceiling
-                    pieces.Add(new SpecialTerrain(325, 90, 20, 20, 40, 120)); //tiny moving platform
-                    pieces.Add(new SpecialTerrain(450, 90, 50, 50, 40, 90)); //bigger moving platform
-                    pieces.Add(new Terrain(400, 140, 500, 30)); //platform that connects to bigger moving platform
-                    pieces.Add(new DeathObject(500, 110, 250, 30)); //YOU SHALL NOT PASS
-                    pieces.Add(new Terrain(500, 450, 275, 40)); //right floor
-                    pieces.Add(new DeathObject(600, 420, 100, 30)); //spikes on right floor
-                    pieces.Add(new Terrain(400, 170, 40, 130)); //wall thing
-                    pieces.Add(new LevelGoal(710, 360, 50, 50)); //Goal
+                    pieces.Add(new Terrain(150, 125, 40, 400)); // first wall
+                    pieces.Add(new Terrain(400, 40, 250, 200)); //top obstacle
+                    pieces.Add(new Terrain(400, 280, 400, 300)); //bottom obstacle
+                    pieces.Add(new Terrain(700, 260, 100, 20)); //platform to shoot
+                    pieces.Add(new LevelGoal(725, 200, 50, 50)); //GOOOOOAAAAAAALLLLLLLL
                     break;
 
             }
